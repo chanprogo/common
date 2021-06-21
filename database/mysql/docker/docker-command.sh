04bc1b00cbe6c7a04d07b9c4157df019a62f37e9
chanprogo <2483777043@qq.com>
2021-06-21 16:03:28 +0800

    .

--- database/mysql/docker/docker-command.sh
+++ database/mysql/docker/docker-command.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name calldb --net=bridge -p 3306:3306 -v /deploy/ccusercenter/mysql-conf/my.cnf:/etc/my.cnf -v /deploy/ccusercenter/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=rootroot -e MYSQL_USER=admin -e MYSQL_PASS=admin mysql/mysql-server
+docker run -d --name calldb --net=bridge -p 3306:3306 -v /root/devware/database/mysql/docker/conf/my.cnf:/etc/my.cnf -v /root/devware/database/mysql/docker/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=toor -e MYSQL_USER=admin -e MYSQL_PASS=admin mysql/mysql-server
